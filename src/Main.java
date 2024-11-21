be114d52b4f0f1cc3c2e6e5e1c165fa3090a7174
Frenzy017 <aleksladjov4@gmail.com>
2024-11-21 13:02:09 +0200

    Update repository

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Store store = new Store();
-        Utility utility = new Utility();
-
-        utility.printWelcomeMessage();
-        store.start();
-    }
-}
