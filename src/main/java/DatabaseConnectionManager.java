95ec83d0b94abf6c0a7ac64195c115e745ab3104
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-11-29 14:13:09 +0200

    Create DatabaseConnectionManager to configure seperate databases names

---
+++ src/main/java/DatabaseConnectionManager.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+    private static final String URL_TEMPLATE = "jdbc:mysql://localhost:3306/%s";
+    private static final String USER = "root";
+    private static final String PASSWORD = "alexbi123";
+
+    public Connection getConnection(String databaseName) throws SQLException {
+        String url = String.format(URL_TEMPLATE, databaseName);
+        return DriverManager.getConnection(url, USER, PASSWORD);
+    }
+}
