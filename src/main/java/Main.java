fb1525663c2f3b0932c9af1f3889934433546b37
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-11-29 14:10:18 +0200

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.FileNotFoundException;
-
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args)  {
         Store store = new Store();
         Utility utility = new Utility();
 
