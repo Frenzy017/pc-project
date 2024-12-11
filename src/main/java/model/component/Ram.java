29903718af47425111acca8ccbc326f29154d208
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-11 20:58:38 +0200

    Create Ram

---
+++ src/main/java/model/component/Ram.java
@@ -0,0 +1,33 @@
+package model.component;
+
+public class Ram {
+    private int id;
+    private int capacity;
+    private int quantity;
+    private int price;
+
+
+    public Ram(int id, int capacity, int quantity, int price) {
+        this.id = id;
+        this.capacity = capacity;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
