74a15ba4fdcdf4596212148d6b6c84e5de3d9a82
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-11 21:05:03 +0200

    Create VideoCard

---
+++ src/main/java/model/component/VideoCard.java
@@ -0,0 +1,31 @@
+package model.component;
+
+public class VideoCard {
+    private int id;
+    private String name;
+    private int quantity;
+    private int price;
+
+    public VideoCard(int id, String name, int quantity, int price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
