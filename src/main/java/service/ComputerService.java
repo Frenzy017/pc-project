6253d58314deba20b50f289e35ba6e6c9caea4d1
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-11 20:56:50 +0200

    Update ComputerService

--- src/main/java/service/ComputerService.java
+++ src/main/java/service/ComputerService.java
@@ -3,7 +3,6 @@ package service;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import connection.DatabaseConnectionManager;
 import exception.DatabaseException;
@@ -12,38 +11,61 @@ import model.Computer;
 public class ComputerService {
     private final DatabaseConnectionManager dbManager = new DatabaseConnectionManager();
 
-    public void insertComputerIntoDatabase(Computer computer) {
-        String sql = "INSERT INTO computers (name, graphicCard, ram, processor, price) VALUES (?, ?, ?, ?, ?)";
+    public int insertComputerIntoDatabase(Computer computer) {
+        String sql = "INSERT INTO computers (name, processor_id, ram_id, videoCard_id, totalPrice) VALUES (?, ?, ?, ?, ?)";
+        int generatedId = 0;
 
         try (Connection computersConnection = dbManager.getConnection("computers");
-             PreparedStatement ps = computersConnection.prepareStatement(sql)) {
+             PreparedStatement ps = computersConnection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
             ps.setString(1, computer.getName());
-            ps.setString(2, computer.getGraphicCard());
-            ps.setInt(3, computer.getRam());
-            ps.setString(4, computer.getProcessor());
-            ps.setInt(5, computer.getPrice());
+            ps.setInt(2, computer.getProcessor_id());
+            ps.setInt(3, computer.getRam_id());
+            ps.setInt(4, computer.getVideoCard_id());
+            ps.setInt(5, computer.getTotalPrice());
 
             ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                generatedId = rs.getInt(1);
+            }
         } catch (SQLException e) {
             throw new DatabaseException("Error inserting computer in database: " + computer, e);
         }
+
+        return generatedId;
     }
 
     public void updateComputerInDatabase(Computer computer) {
-        String sql = "UPDATE computers SET name = ?, graphicCard = ?, ram = ?, processor = ?, price = ? WHERE id = ?";
+        String sql = "UPDATE computers SET name = ?, processor_id = ?, ram_id = ?, videoCard_id = ?, totalPrice = ? WHERE id = ?";
 
         try (Connection conn = dbManager.getConnection("computers");
              PreparedStatement ps = conn.prepareStatement(sql)) {
 
             ps.setString(1, computer.getName());
-            ps.setString(2, computer.getGraphicCard());
-            ps.setInt(3, computer.getRam());
-            ps.setString(4, computer.getProcessor());
-            ps.setInt(5, computer.getPrice());
+            ps.setInt(2, computer.getVideoCard_id());
+            ps.setInt(3, computer.getRam_id());
+            ps.setInt(4, computer.getProcessor_id());
+            ps.setInt(5, computer.getTotalPrice());
             ps.setInt(6, computer.getId());
 
             ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseException("Error updating computer in database: " + computer, e);
+        }
+    }
+
+    public void updateComputerPriceInDatabase(Computer computer, int buildPrice) {
+        String sql = "UPDATE computers SET totalPrice = ? WHERE id = ?";
+
+        try (Connection conn = dbManager.getConnection("computers");
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, buildPrice);
+            ps.setInt(2, computer.getId());
+            ps.executeUpdate();
+
         } catch (SQLException e) {
             throw new DatabaseException("Error updating computer in database: " + computer, e);
         }
@@ -65,7 +87,7 @@ public class ComputerService {
 
     public List<Computer> getAllComputerProperties() {
         List<Computer> computers = new ArrayList<>();
-        String sql = "SELECT id, name, graphicCard, ram, processor, price FROM computers";
+        String sql = "SELECT id, name, processor_id, ram_id, videoCard_id, totalPrice FROM computers";
 
         try (Connection conn = dbManager.getConnection("computers");
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -75,10 +97,10 @@ public class ComputerService {
                 Computer computer = new Computer(
                         rs.getInt("id"),
                         rs.getString("name"),
-                        rs.getString("graphicCard"),
-                        rs.getInt("ram"),
-                        rs.getString("processor"),
-                        rs.getInt("price")
+                        rs.getInt("processor_id"),
+                        rs.getInt("ram_id"),
+                        rs.getInt("videoCard_id"),
+                        rs.getInt("totalPrice")
                 );
                 computers.add(computer);
             }
