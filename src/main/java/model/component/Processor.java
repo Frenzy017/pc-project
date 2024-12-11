663ba8bb6893c375938692caee80f774ba617b8a
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-11 20:58:11 +0200

    Create Processor

---
+++ src/main/java/model/component/Processor.java
@@ -0,0 +1,32 @@
+package model.component;
+
+public class Processor {
+    private int id;
+    private String name;
+    private int quantity;
+    private int price;
+
+
+    public Processor(int id, String name, int quantity, int price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
