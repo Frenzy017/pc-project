f4ae6c75d0f87c6410dec7ac61add81f77158ef7
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-13 08:14:41 +0200

    Update RamHandler

--- src/main/java/handler/component/RamHandler.java
+++ src/main/java/handler/component/RamHandler.java
@@ -24,6 +24,15 @@ public class RamHandler {
     }
 
     public void handleRamConfig() {
+        printRamOptions();
+
+        System.out.println("You have selected then following ram capacity: " + ramObject.getCapacity() + "," + " Price: " + ramObject.getPrice() + "$");
+        System.out.println();
+        System.out.println("Please proceed further with the other options to complete your build!");
+        System.out.println();
+    }
+
+    public void printRamOptions() {
         List<Ram> ram = ramService.getAllRam();
 
         System.out.println("Here is the available ram capacity: ");
@@ -38,11 +47,24 @@ public class RamHandler {
 
         int ramChoice = scanner.nextInt();
         ramObject = ram.get(ramChoice - 1);
+    }
 
-        System.out.println("You have selected then following ram capacity: " + ramObject.getCapacity() + "," + " Price: " + ramObject.getPrice() + "$");
+    public void printRamQuantity() {
+        List<Ram> rams = ramService.getAllRam();
+
         System.out.println();
-        System.out.println("Please proceed further with the other options to complete your build!");
+        System.out.println("Here are the available RAMs: ");
+
+        int index = 1;
+        for (Ram ram : rams) {
+            System.out.println(index++ + ". RAM: " + ram.getCapacity() + "GB - Quantity: " + ram.getQuantity());
+        }
+
         System.out.println();
+        System.out.print("Please choose a RAM: ");
+
+        int ramChoice = scanner.nextInt();
+        ramObject = rams.get(ramChoice - 1);
     }
 
     public Ram getRamObject() {
