e5ed4ea40af9f57e5539deb54c4351f1845c9600
Aleksandar Ladzhov <aleksladjov4@gmail.com>
2024-12-13 08:12:50 +0200

    Add setQuantity and SetId methods

--- src/main/java/model/component/Processor.java
+++ src/main/java/model/component/Processor.java
@@ -6,7 +6,6 @@ public class Processor {
     private int quantity;
     private int price;
 
-
     public Processor(int id, String name, int quantity, int price) {
         this.id = id;
         this.name = name;
@@ -28,5 +27,13 @@ public class Processor {
 
     public int getPrice() {
         return price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setId(int generatedId) {
+        this.id = generatedId;
     }
 }
